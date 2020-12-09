e6bae7e3afb128cb1931e5c77d9cb8571bf11030
ljj <904990231@qq.com>
2020-12-09 20:21:03 +0800

    2020/12/09/20：19 李佳俊 添加：添加按钮 完善管理员界面中班级、学生、教师信息界面排版和切换功能

--- src/main/webapp/resources/user/admin/adminFunction.js
+++ src/main/webapp/resources/user/admin/adminFunction.js
@@ -1,20 +1,23 @@
 window.onload = function(){
-    var classBtn = document.getElementById("classInformation");
-    var classData = document.getElementById("classData");
-    var teacherBtn = document.getElementById("teacherInformation");
-    var teacherData = document.getElementById("teacherDate");
-    var studentBtn = document.getElementById("studentInformation");
-    var studentData = document.getElementById("studentDate");
-    var noticeBtn = document.getElementById("notice");
-    var noticeData = document.getElementById("noticeDate");
-    var messageBtn = document.getElementById("messageBoard");
-    var messageData = document.getElementById("messageDate");
+    let classBtn = document.getElementById("classInformation");
+    let classData = document.getElementById("classData");
+    let teacherBtn = document.getElementById("teacherInformation");
+    let teacherData = document.getElementById("teacherDate");
+    let studentBtn = document.getElementById("studentInformation");
+    let studentData = document.getElementById("studentDate");
+    let noticeBtn = document.getElementById("notice");
+    let noticeData = document.getElementById("noticeDate");
+    let messageBtn = document.getElementById("messageBoard");
+    let messageData = document.getElementById("messageDate");
+    let resetPasswordBtn = document.getElementById("resetPasswordBtn");
+    let resetPassword = document.getElementById("resetPassword");
     classBtn.onclick = function(){
         classData.style.display = "block";
         teacherData.style.display = "none";
         studentData.style.display = "none";
         noticeData.style.display = "none";
         messageData.style.display = "none";
+        resetPassword.style.display = "none";
     }
     teacherBtn.onclick = function(){
         classData.style.display = "none";
@@ -22,6 +25,7 @@ window.onload = function(){
         studentData.style.display = "none";
         noticeData.style.display = "none";
         messageData.style.display = "none";
+        resetPassword.style.display = "none";
     }
     studentBtn.onclick = function(){
         classData.style.display = "none";
@@ -29,6 +33,7 @@ window.onload = function(){
         studentData.style.display = "block";
         noticeData.style.display = "none";
         messageData.style.display = "none";
+        resetPassword.style.display = "none";
     }
     noticeBtn.onclick = function(){
         classData.style.display = "none";
@@ -36,6 +41,7 @@ window.onload = function(){
         studentData.style.display = "none";
         noticeData.style.display = "block";
         messageData.style.display = "none";
+        resetPassword.style.display = "none";
     }
     messageBtn.onclick = function(){
         classData.style.display = "none";
@@ -43,5 +49,14 @@ window.onload = function(){
         studentData.style.display = "none";
         noticeData.style.display = "none";
         messageData.style.display = "block";
+        resetPassword.style.display = "none";
+    }
+    resetPasswordBtn.onclick = function(){
+        classData.style.display = "none";
+        teacherData.style.display = "none";
+        studentData.style.display = "none";
+        noticeData.style.display = "none";
+        messageData.style.display = "none";
+        resetPassword.style.display = "block";
     }
 }
