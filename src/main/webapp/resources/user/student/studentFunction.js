a26c3797421bc55856e48a274eb3d7ae1a2bf483
ljj <904990231@qq.com>
2020-12-11 14:00:26 +0800

    2020/12/11/14:00 李佳俊 添加：学生公告界面测试用数据

--- src/main/webapp/resources/user/student/studentFunction.js
+++ src/main/webapp/resources/user/student/studentFunction.js
@@ -1,3 +1,20 @@
+let messageDate = [
+    {
+        title: "第一个标题",
+        text: "第一个内容",
+        time: "第一个时间"
+    },
+    {
+        title: "第二个标题",
+        text: "第二个内容",
+        time: "第二个时间"
+    },
+    {
+        title: "第三个标题",
+        text: "第三个内容",
+        time: "第三个时间"
+    }
+]
 window.onload = function () {
     let homeworkBtn = document.getElementById("homeworkInformation"),
         homeworkData = document.getElementById("homeworkData"),
@@ -27,16 +44,38 @@ window.onload = function () {
         resetPassword.style.display = "none";
     }
     resetPasswordBtn.onclick = function () {
-        homeworkData.style.display = "none";
-        messageData.style.display = "none";
-        noticeData.style.display = "none";
-        resetPassword.style.display = "block";
+            homeworkData.style.display = "none";
+            messageData.style.display = "none";
+            noticeData.style.display = "none";
+            resetPassword.style.display = "block";
+        }
+    for(let i = 0;i<messageDate.length;i++){
+        let li1 = document.createElement('li');
+        li1.innerHTML = '<div class="row"><div class="col-md-2">'+messageDate[i].title+'</div><div class="col-md-8">'+messageDate[i].text+'</div><div class="col-md-2">'+messageDate[i].time+'</div></div>'
+        document.getElementById("messageDateList").appendChild(li1);
+
     }
     for (let j = 0;j < listColor.length;j++){
-        for (var i = 0;i < listColor[j].children.length;i++){
+        for (let i = 0;i < listColor[j].children.length;i++){
             if (i%2==1){
                 listColor[j].children[i].children[0].style.backgroundColor = "white"
             }
         }
     }
+    // 鼠标移入出现弹窗
+    // homeworkBtn.onmouseover = function () {
+    //     console.log("11")
+    // }
+    // for (let i = 0;i < 3;i++){
+    //     btn[i].onclick = function () {
+    //         for (let j = 0;j < 3;j++){
+    //             if (j==i){
+    //                 data[j].style.display = "block";
+    //             }else{
+    //                 data[j].style.display = "none";
+    //             }
+    //         }
+    //     }
+    // }
+
 }
